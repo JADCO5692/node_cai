63303d5a9a8e61f0b6763abe2ed3a2ba31629b78
himanshumittal <himanshu5692@gmail.com>
2020-01-31 12:08:31 +0530

    Multiple message handling

--- index.js
+++ index.js
@@ -8,51 +8,55 @@ const port =  process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
-app.post('/sapcai',(req,res) => {
-    const response = res;
+app.post('/sapcai',(req,res3) => {
+    const response = res3;
     const msg = req.body.queryResult.queryText;
     const sessionID = req.body.session.substr(36,115);
     var build = new sapcai.build('5c9a27df8e428b5f449c0df8a038bb88', 'en')
-    var detectlang , transtext, cairesponse;
+    var detectlang , transtext, cairesponse, i;
+	var response_text = {
+	  "payload": {
+		"google": {
+		  "expectUserResponse": true,
+		  "richResponse": {
+			"items": [						  
+			]
+		  }
+		}
+	  }
+	};
      //google translate code start.
-  translate(msg, { to: 'en' }).then(res => {
+  translate(msg, { to: 'en' }).then(res2 => {
  
-    detectlang = res.from.language.iso;
-    transtext = res.text;
+    detectlang = res2.from.language.iso;
+    transtext = res2.text;
     build.dialog({ type: 'text', content: transtext}, { conversationId: sessionID })
-     .then(res => {
-		 
+     .then(res => {					
 		for (i = 0; i < res.messages.length; i++) 
 		{
 			if(res.messages[i].content != 'The  is ')
 			{
 				cairesponse = res.messages[i].content;
 
-				translate(cairesponse, { to: detectlang }).then(res => {
-				//response.send({fulfillmentText: res.text})
-				response.send({
-				  "payload": {
-					"google": {
-					  "expectUserResponse": true,
-					  "richResponse": {
-						"items": [
-						  {
-							"simpleResponse": {
-							  "textToSpeech": res.text
+				translate(cairesponse, { to: detectlang }).then(res1 => {
+					response_text.payload.google.richResponse.items.push(
+						{
+							"simpleResponse": 
+							{
+								"textToSpeech": res1.text
 							}
-						  }
-						]
-					  }
+						})
+					console.log(JSON.stringify(response_text));
+					console.log(res1.text); 
+					if(i == res.messages.length)
+					{
+						response.send(response_text);
 					}
-				  }
-				})
-
-				console.log(res.text); 
 				}).catch(err => {
 				console.error(err);
-				});
+				});							
 			}
-		}
+		}		
   }).catch(err => {
     console.error(err);
   });
